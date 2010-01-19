f7cc15c080fe9338706169958cba91151202c64d
Felipe Coury <felipe.coury@gmail.com>
2010-01-19 13:38:12 -0200

    Added named_options for allowing things like `wn command --options1=abc --option2'

--- lib/wn.rb
+++ lib/wn.rb
@@ -12,7 +12,7 @@ module Wn
   VERSION = '0.1.2'
   
   class App
-    attr_accessor :input, :command, :options
+    attr_accessor :input, :command, :options, :named_options
     
     include Wn::Helpers
     include Wn::Commands
@@ -20,15 +20,29 @@ module Wn
     # Initializes the Webbynode App
     def initialize(*input)
       @input = input.flatten
+      @named_options = {}
+      @options = []
     end
     
     # Parses user input (commands)
     # Initial param is the command
-    # Other params are arguments
+    # Other params are named parameters (like 'command --this=param')
+    # or options (like 'command param')
     def parse_command
       log_and_exit read_template('help') if @input.empty?
       @command  = @input.shift
-      @options  = @input
+      
+      while @input.any?
+        opt = @input.shift
+        
+        if opt =~ /^--(\w+)(=("[^"]+"|[\w]+))*/
+          name  = $1
+          value = $3 ? $3.gsub(/"/, "") : true
+          @named_options[name] = value
+        else
+          @options << opt
+        end
+      end
     end
     
     # Executes the parsed command
@@ -40,7 +54,7 @@ module Wn
     # Runs the command unless it's nil or doesn't exist
     # If it fails, it will display the help screen
     def run_command(command)
-      if !command.nil? and respond_to?(command)
+      if command and respond_to?(command)
         send(command)
       else
         log_and_exit read_template('help')

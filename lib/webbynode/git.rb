4a37a3d20792585e2dd3f7f45279dacd234970d0
Felipe Coury <felipe.coury@gmail.com>
2010-01-21 11:53:32 -0200

    Improved git specs.

--- lib/webbynode/git.rb
+++ lib/webbynode/git.rb
@@ -1,4 +1,5 @@
 module Webbynode
+  class GitError < StandardError; end
   class GitNotRepoError < StandardError; end
   class GitRemoteAlreadyExistsError < StandardError; end
 
@@ -12,27 +13,42 @@ module Webbynode
     end
     
     def init
-      if @io.exec("git init") =~ /^Initialized empty Git repository in/
-        true
-      else
-        false
+      exec("git init") do |output|
+        # this indicates init was properly executed
+        output =~ /^Initialized empty Git repository in/
       end
     end
     
     def add(what)
-      @io.exec "git add #{what}"
+      exec "git add #{what}"
     end
     
     def add_remote(name, host, repo)
-      output = @io.exec("git remote add #{name} git@#{host}:#{repo}")
-      
-      if output =~ /Not a git repository/
-        raise GitNotRepoError, output
-      elsif output =~ /remote \w+ already exists/
-        raise GitRemoteAlreadyExistsError, output
+      exec("git remote add #{name} git@#{host}:#{repo}") do |output|
+        # raise an exception if remote already exists
+        raise GitRemoteAlreadyExistsError, output if output =~ /remote \w+ already exists/
+        
+        # success if output is empty
+        output.blank?
+      end
+    end
+    
+    private
+    
+    def exec(cmd, &blk)
+      handle_output @io.exec(cmd), &blk
+    end
+    
+    def handle_output(output, &blk)
+      raise GitNotRepoError, output if output =~ /Not a git repository/
+
+      if blk
+        raise GitError, output unless blk.call(output)
+      else
+        raise GitError, output unless output.blank?
       end
       
-      output.blank?
+      true
     end
   end
 end
--- spec/git_spec.rb
+++ spec/git_spec.rb
@@ -21,81 +21,108 @@ describe Webbynode::Git do
   end
   
   describe "init" do
-    it "should create a new git repo" do
-      io_handler = mock("io")
-      io_handler.should_receive(:exec).with("git init")
-      
-      git = Webbynode::Git.new(io_handler)
-      git.init
+    context "when successfull" do
+      it "should return true" do
+        io_handler = mock("io")
+        io_handler.should_receive(:exec).with("git init").and_return("Initialized empty Git repository in /Users/fcoury/tmp/.git/")
+
+        git = Webbynode::Git.new(io_handler)
+        git.init.should == true
+      end
     end
     
-    it "should return true if repository is created" do
-      io_handler = mock("io")
-      io_handler.should_receive(:exec).with("git init").and_return("Initialized empty Git repository in /Users/fcoury/tmp/.git/")
-      
-      git = Webbynode::Git.new(io_handler)
-      git.init.should == true
-    end
-    
-    it "should return false if there's an error creating the repo" do
-      io_handler = mock("io")
-      io_handler.should_receive(:exec).with("git init").and_return("/Users/fcoury/tmp/other/.git: Permission denied")
-      
-      git = Webbynode::Git.new(io_handler)
-      git.init.should == false
+    context "when unsuccessfull" do
+      it "should raise exception if not a git repo" do
+        io_handler = mock("io")
+        io_handler.should_receive(:exec).with("git init").and_return("fatal: Not a git repository (or any of the parent directories): .git")
+
+        git = Webbynode::Git.new(io_handler)
+        lambda { git.init }.should raise_exception(Webbynode::GitNotRepoError)
+      end
+
+      it "should raise a generic Git error if there's another error creating the repo" do
+        io_handler = mock("io")
+        io_handler.should_receive(:exec).with("git init").and_return("/Users/fcoury/tmp/other/.git: Permission denied")
+
+        git = Webbynode::Git.new(io_handler)
+        lambda { git.init }.should raise_exception(Webbynode::GitError)
+      end
     end
   end
   
   describe "add_remote" do
-    it "should create a new remote" do
-      io_handler = mock("io")
-      io_handler.should_receive(:exec).with("git remote add webbynode git@1.2.3.4:the_repo").and_return("")
-      
-      git = Webbynode::Git.new(io_handler)
-      git.add_remote("webbynode", "1.2.3.4", "the_repo").should == true
+    context "when successfull" do
+      it "should create a new remote" do
+        io_handler = mock("io")
+        io_handler.should_receive(:exec).with("git remote add webbynode git@1.2.3.4:the_repo").and_return("")
+
+        git = Webbynode::Git.new(io_handler)
+        git.add_remote("webbynode", "1.2.3.4", "the_repo").should == true
+      end
     end
     
-    it "should raise exception if not a git repo" do
-      io_handler = mock("io")
-      io_handler.should_receive(:exec).with("git remote add other git@5.6.7.8:a_repo").and_return("fatal: Not a git repository (or any of the parent directories): .git")
+    context "when unsuccessfull" do
+      it "should raise exception if not a git repo" do
+        io_handler = mock("io")
+        io_handler.should_receive(:exec).with("git remote add other git@5.6.7.8:a_repo").and_return("fatal: Not a git repository (or any of the parent directories): .git")
 
-      git = Webbynode::Git.new(io_handler)
-      lambda { git.add_remote("other", "5.6.7.8", "a_repo") }.should raise_exception(Webbynode::GitNotRepoError)
-    end
+        git = Webbynode::Git.new(io_handler)
+        lambda { git.add_remote("other", "5.6.7.8", "a_repo") }.should raise_exception(Webbynode::GitNotRepoError)
+      end
     
-    it "should return raise exception if the remote already exists" do
-      io_handler = mock("io")
-      io_handler.should_receive(:exec).and_return("fatal: remote webbynode already exists.")
+      it "should return raise exception if the remote already exists" do
+        io_handler = mock("io")
+        io_handler.should_receive(:exec).and_return("fatal: remote webbynode already exists.")
 
-      git = Webbynode::Git.new(io_handler)
-      lambda { git.add_remote("other", "5.6.7.8", "a_repo") }.should raise_exception(Webbynode::GitRemoteAlreadyExistsError)
+        git = Webbynode::Git.new(io_handler)
+        lambda { git.add_remote("other", "5.6.7.8", "a_repo") }.should raise_exception(Webbynode::GitRemoteAlreadyExistsError)
+      end
+
+      it "should raise a generic Git error when another error occurs" do
+        io_handler = mock("io")
+        io_handler.should_receive(:exec).and_return("/Users/fcoury/tmp/other/.git: Permission denied")
+
+        git = Webbynode::Git.new(io_handler)
+        lambda { git.add_remote("other", "5.6.7.8", "a_repo") }.should raise_exception(Webbynode::GitError)
+      end
     end
   end
   
   describe "add" do
-    it "should add objects to git" do
-      io_handler = mock("io")
-      io_handler.should_receive(:exec).with('git add the_file')
+    context "when successfull" do
+      it "should add objects to git" do
+        io_handler = mock("io")
+        io_handler.should_receive(:exec).with('git add the_file')
       
-      git = Webbynode::Git.new(io_handler)
-      git.add("the_file")
+        git = Webbynode::Git.new(io_handler)
+        git.add("the_file")
+      end
+
+      it "should handle adding multiple files" do
+        io_handler = mock("io")
+        io_handler.should_receive(:exec).with('git add one_file/ other_file/')
+      
+        git = Webbynode::Git.new(io_handler)
+        git.add("one_file/ other_file/")
+      end
     end
 
-    it "should specified files" do
-      io_handler = mock("io")
-      io_handler.should_receive(:exec).with('git add one_file/ other_file/')
-      
-      git = Webbynode::Git.new(io_handler)
-      git.add("one_file/ other_file/")
-    end
-    
-    it "should return false if there's an error creating the repo" do
-      pending
-      io_handler = mock("io")
-      io_handler.should_receive(:exec).with("git remote add other git@5.6.7.8:a_repo").and_return("fatal: Not a git repository (or any of the parent directories): .git")
+    context "when unsuccessfull" do
+      it "should raise exception if not a git repo" do
+        io_handler = mock("io")
+        io_handler.should_receive(:exec).with("git add .").and_return("fatal: Not a git repository (or any of the parent directories): .git")
 
-      git = Webbynode::Git.new(io_handler)
-      git.add_remote("other", "5.6.7.8", "a_repo").should == false
+        git = Webbynode::Git.new(io_handler)
+        lambda { git.add(".") }.should raise_exception(Webbynode::GitNotRepoError)
+      end
+
+      it "should raise a generic Git error when another error occurs" do
+        io_handler = mock("io")
+        io_handler.should_receive(:exec).and_return("/Users/fcoury/tmp/other/.git: Permission denied")
+
+        git = Webbynode::Git.new(io_handler)
+        lambda { git.add("something") }.should raise_exception(Webbynode::GitError)
+      end
     end
   end
 

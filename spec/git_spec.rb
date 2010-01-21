dcc1f963801bc814ff2ba5731a5e7c3ea65ac1d9
Felipe Coury <felipe.coury@gmail.com>
2010-01-21 12:40:51 -0200

    DRYed out failure git specs

--- spec/git_spec.rb
+++ spec/git_spec.rb
@@ -2,12 +2,27 @@
 require File.join(File.expand_path(File.dirname(__FILE__)), 'spec_helper')
 
 describe Webbynode::Git do
-  def should_raise_giterror(command)
+  def should_raise_when_response(exception, command, response, &blk)
     io_handler = mock("io")
-    io_handler.should_receive(:exec).with(command).and_return("fatal: Not a git repository (or any of the parent directories): .git")
+    io_handler.should_receive(:exec).with(command).and_return(response)
 
     git = Webbynode::Git.new(io_handler)
-    lambda { yield git }.should raise_exception(Webbynode::GitNotRepoError)
+    lambda { yield git }.should raise_exception(exception)
+  end
+
+  def should_raise(exception, command, &blk)
+    should_raise_when_response exception, command, 
+      "fatal: remote webbynode already exists.", &blk
+  end
+
+  def should_raise_giterror(command, &blk)
+    should_raise_when_response Webbynode::GitError, command, 
+      "/private/tmp/other/.git: Permission denied", &blk
+  end
+
+  def should_raise_notgitrepo(command, &blk)
+    should_raise_when_response Webbynode::GitNotRepoError, command, 
+      "fatal: Not a git repository (or any of the parent directories): .git", &blk
   end
 
   describe "present?" do
@@ -41,11 +56,7 @@ describe Webbynode::Git do
     
     context "when unsuccessfull" do
       it "should raise exception if not a git repo" do
-        io_handler = mock("io")
-        io_handler.should_receive(:exec).with("git init").and_return("fatal: Not a git repository (or any of the parent directories): .git")
-
-        git = Webbynode::Git.new(io_handler)
-        lambda { git.init }.should raise_exception(Webbynode::GitNotRepoError)
+        should_raise_notgitrepo("git init") { |git| git.init }
       end
 
       it "should raise a generic Git error if there's another error creating the repo" do
@@ -67,21 +78,15 @@ describe Webbynode::Git do
     
     context "when unsuccessfull" do
       it "should raise exception if not a git repo" do
-        io_handler = mock("io")
-        io_handler.should_receive(:exec).with("git remote add other git@5.6.7.8:a_repo").and_return("fatal: Not a git repository (or any of the parent directories): .git")
-
-        git = Webbynode::Git.new(io_handler)
-        lambda { git.add_remote("other", "5.6.7.8", "a_repo") }.should raise_exception(Webbynode::GitNotRepoError)
+        should_raise_notgitrepo("git remote add other git@5.6.7.8:a_repo") { |git| git.add_remote("other", "5.6.7.8", "a_repo") }
       end
     
       it "should return raise exception if the remote already exists" do
-        io_handler = mock("io")
-        io_handler.should_receive(:exec).and_return("fatal: remote webbynode already exists.")
-
-        git = Webbynode::Git.new(io_handler)
-        lambda { git.add_remote("other", "5.6.7.8", "a_repo") }.should raise_exception(Webbynode::GitRemoteAlreadyExistsError)
-      end
-
+        should_raise(Webbynode::GitRemoteAlreadyExistsError, "git remote add other git@5.6.7.8:a_repo") { |git| 
+          git.add_remote("other", "5.6.7.8", "a_repo")
+        }
+      end  
+      
       it "should raise a generic Git error when another error occurs" do
         should_raise_giterror("git remote add other git@5.6.7.8:a_repo") { |git| git.add_remote("other", "5.6.7.8", "a_repo") }
       end
@@ -106,14 +111,10 @@ describe Webbynode::Git do
         git.add("one_file/ other_file/")
       end
     end
-
+    
     context "when unsuccessfull" do
       it "should raise exception if not a git repo" do
-        io_handler = mock("io")
-        io_handler.should_receive(:exec).with("git add .").and_return("fatal: Not a git repository (or any of the parent directories): .git")
-
-        git = Webbynode::Git.new(io_handler)
-        lambda { git.add(".") }.should raise_exception(Webbynode::GitNotRepoError)
+        should_raise_notgitrepo("git add .") { |git| git.add(".") }
       end
 
       it "should raise a generic Git error when another error occurs" do

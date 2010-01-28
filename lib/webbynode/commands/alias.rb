4826f5e6417185fcf2e71fffde9621970b7de15e
Michael van Rooijen <meskyanichi@gmail.com>
2010-01-28 13:31:08 +0100

    Base functionality has been added for adding aliases.

--- lib/webbynode/commands/alias.rb
+++ lib/webbynode/commands/alias.rb
@@ -3,33 +3,72 @@ module Webbynode::Commands
     
     requires_initialization!
     
-    attr_accessor :action, :command #, :session_file, :session_aliases
+    attr_accessor :action, :alias, :command, :session_aliases
     
     parameter :action,  String, "add, remove or show.", :required => true
-    parameter :command, Array,  "Task to perform.",     :required => true
+    parameter :alias,   String, "The custom alias.",    :required => true
+    parameter :command, Array,  "Task to perform.",     :required => false
     
-    File = ".webbynode/aliases"
+    FilePath = ".webbynode/aliases"
+    
+    def initialize(*args)
+      super
+      @session_aliases = Array.new
+    end
     
     def execute
+      
       ensure_aliases_file_exists
+      
+      read_aliases_file
       
       parse_parameters
       
+      send(action)
+      
     end
 
-
+    
+    def read_aliases_file
+      @session_aliases = Array.new
+      io.read_file(FilePath).each_line do |line|
+        if line =~ /\[(.+)\] (.+)/
+          @session_aliases << "[#{$1}] #{$2}"
+        end
+      end
+    end
     
     private
     
       def ensure_aliases_file_exists
-        unless io.file_exists?(File)
-          io.exec("touch #{File}")
+        unless io.file_exists?(FilePath)
+          io.exec("touch #{FilePath}")
         end
       end
       
       def parse_parameters
-        @action   = param(:action)
-        @command  = param(:command).join(" ")
+        @action  = param(:action)
+        @alias   = param(:alias)
+        @command = param(:command).join(" ")
+      end
+
+      def add
+        append_alias
+        write_aliases
+      end
+      
+      def write_aliases
+        io.open_file(FilePath, "w") do |file|
+          session_aliases.each do |a|
+            file << a
+          end
+        end
+      end
+
+      def append_alias
+        unless command.blank?
+          @session_aliases << "[#{@alias}] #{command}"
+        end
       end
 
   end
--- spec/webbynode/commands/alias_spec.rb
+++ spec/webbynode/commands/alias_spec.rb
@@ -3,46 +3,98 @@ require File.join(File.expand_path(File.dirname(__FILE__)), '../..', 'spec_helpe
 
 describe Webbynode::Commands::Alias do
 
-  let(:a) { Webbynode::Commands::Alias.new("add", "ls -la") }
+  let(:a) { Webbynode::Commands::Alias.new("add", "my_alias", "ls", "-la") }
   let(:io) { double('io').as_null_object }
 
   before(:each) do
     a.should_receive(:io).any_number_of_times.and_return(io)
+    File.stub!(:read).with(Webbynode::Commands::Alias::FilePath).
+      and_return(File.join(File.dirname(__FILE__), '../../fixtures/aliases'))
   end
   
   it "should have a constant pointing at the aliases file" do
-    Webbynode::Commands::Alias::File.should eql(".webbynode/aliases")
+    Webbynode::Commands::Alias::FilePath.should eql(".webbynode/aliases")
   end
   
   describe "params" do
-    it "should parse the commands" do
+    it "should parse the add commands" do
+      a = Webbynode::Commands::Alias.new("add", "my_alias", "ls", "-la")
+      a.stub!(:send).with("add")
       a.execute
       a.action.should eql("add")
+      a.alias.should eql("my_alias")
       a.command.should eql("ls -la")
+    end
+    
+    it "should parse the remove commands" do
+      a = Webbynode::Commands::Alias.new("remove", "my_alias")
+      a.stub!(:send).with("remove")
+      a.execute
+      a.action.should eql("remove")
+      a.alias.should eql("my_alias")
+      a.command.should be_blank
     end
   end
   
   describe "aliases file availability" do
     context "when the aliases file is not present" do
       it "should create it" do
-        io.should_receive(:file_exists?).with(Webbynode::Commands::Alias::File).and_return(false)
-        io.should_receive(:exec).with("touch #{Webbynode::Commands::Alias::File}")
+        io.should_receive(:file_exists?).with(Webbynode::Commands::Alias::FilePath).and_return(false)
+        io.should_receive(:exec).with("touch #{Webbynode::Commands::Alias::FilePath}")
         a.execute
       end
     end
     
     context "when the aliases file is present" do
       it "should not create it, nor overwrite it" do
-        io.should_receive(:file_exists?).with(Webbynode::Commands::Alias::File).and_return(true)
-        io.should_not_receive(:exec).with("touch #{Webbynode::Commands::Alias::File}")
+        io.should_receive(:file_exists?).with(Webbynode::Commands::Alias::FilePath).and_return(true)
+        io.should_not_receive(:exec).with("touch #{Webbynode::Commands::Alias::FilePath}")
         a.execute
       end
     end
   end
   
+  describe "reading out the aliases from the file" do
+    it "should read out each line that has an alias" do
+      a.should_receive(:read_aliases_file)
+      a.execute
+    end
+  end
+  
   describe "aliases file interaction" do
     context "when writing to the file" do
+      before(:each) do
+      end
       
+      it "should invoke the add method" do
+        a.should_receive(:send).with('add')
+        a.execute
+      end
+      
+      it "should append the new alias to the session_aliases" do
+        a.should_receive(:append_alias)
+        a.execute
+      end
+      
+      it "should open the file in 'write mode' to write all aliases from session_aliases to it" do
+        io.should_receive(:open_file).with(Webbynode::Commands::Alias::FilePath, "w")
+        a.execute
+      end
+      
+      it "should not add the alias to session_aliases if the command is blank" do
+        a = Webbynode::Commands::Alias.new("add", "my_alias", "custom command")
+        a.stub!(:write_aliases)
+        a.execute
+        a.session_aliases.size.should eql(1)
+        a.session_aliases[0].should eql("[my_alias] custom command")
+      end
+      
+      it "should not add the alias to session_aliases if the command is blank" do
+        a = Webbynode::Commands::Alias.new("add", "my_alias")
+        a.stub!(:write_aliases)
+        a.execute
+        a.session_aliases.size.should eql(0)
+      end
     end
   end
   

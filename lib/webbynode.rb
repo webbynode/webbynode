7fe5e3091c31e4920b49ae6d9e358266f853e15f
Felipe Coury <felipe.coury@gmail.com>
2010-09-22 18:02:24 -0300

    Bumped version to 1.0.3.

--- lib/webbynode.rb
+++ lib/webbynode.rb
@@ -56,7 +56,7 @@ require File.join(File.dirname(__FILE__), 'webbynode', 'commands', 'user')
 require File.join(File.dirname(__FILE__), 'webbynode', 'application')
 
 module Webbynode
-  VERSION = '1.0.3.beta4'
+  VERSION = '1.0.3'
 end
 
 class Array
